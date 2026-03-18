a2276c76b65b34e0bb1b2c36e1b8cd8cfa5a6b40
Jeroen van Riel <jeroenvanriel@outlook.com>
2026-03-18 11:19:38 +0100

    draw sumo network as polygons
    - Include junction shapes
    - Support polygons with holes
    - Ignore internal edges/lanes (inside junctions)

--- backend/app/network.py
+++ backend/app/network.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter
 import xml.etree.ElementTree as ET
-from shapely.geometry import LineString
-from shapely.ops import unary_union
+from shapely.geometry import LineString, Polygon
+from shapely import MultiPolygon
 
 from app.util import get_scene_root
 
@@ -12,32 +12,54 @@ async def get_road_network(scene_id: str):
     road_file = get_scene_root() / scene_id / "road.net.xml"
     tree = ET.parse(road_file)
     root = tree.getroot()
+    polygons = []
 
-    merged_roads = []
+    def parse_SUMO_coords(shape_str):
+        # NOTE: we need to flip y-coords for SUMO
+        points = [tuple(map(float, p.split(","))) for p in shape_str.split()]
+        return [(x, -y) for (x, y) in points]
 
     for edge in root.findall("edge"):
+        # no need to draw lanes within junctions
+        if edge.get("function") == "internal":
+            continue
+
         lanes = edge.findall("lane")
-        lane_shapes = []
         for lane in lanes:
             shape_str = lane.get("shape")
-            width = float(lane.get("width", 3.0))
-            points = [tuple(map(float, p.split(","))) for p in shape_str.split()]
+            points = parse_SUMO_coords(shape_str)
+            if len(points) == 0: # ignore invalid lines
+                continue
+            shape = LineString(points)
 
-            # NOTE: we need to flip y-coords (SUMO) in the bounds
-            points = [(x, -y) for (x, y) in points]
-
-            line = LineString(points)
             # buffer by half width to create polygon
-            polygon = line.buffer(width / 2, cap_style=2, join_style=2)
-            lane_shapes.append(polygon)
-        # merge all lanes of this edge
-        merged_roads.append(unary_union(lane_shapes))
+            width = float(lane.get("width", 3.0))
+            polygon = shape.buffer(width / 2, cap_style=1, join_style=1)
+            if not polygon.is_empty:
+                polygons.append(polygon)
 
-    all_polygons = unary_union(merged_roads)  # merge into single geometry
-    minx, miny, maxx, maxy = all_polygons.bounds
-    pad = 10 # add a little padding on the edges of the network
+    for junction in root.findall("junction"):
+        shape_str = junction.get("shape")
+        if shape_str is None:
+            continue
+        points = parse_SUMO_coords(shape_str)
+        if len(points) <= 2: # ignore invalid polygons
+            continue
+        shape = Polygon(points)
+        polygons.append(shape)
 
+    # compute bounds of all polygons together
+    minx, miny, maxx, maxy = MultiPolygon(polygons).bounds
+    pad = 10 # extra padding for the bounds
     return {
-        "polygons": [[dict(x=x, y=y) for x, y in p.exterior.coords] for p in merged_roads],
+        "polygons": [
+            {
+                "outer": [dict(x=x, y=y) for x, y in p.exterior.coords],
+                "holes": [
+                    [dict(x=x, y=y) for x, y in interior.coords]
+                    for interior in p.interiors
+                ],
+            }
+            for p in polygons],
         "bounds": dict(minx=minx - pad, miny=miny - pad, maxx=maxx + pad, maxy=maxy + pad),
     }
--- frontend/src/components/Road.tsx
+++ frontend/src/components/Road.tsx
@@ -3,7 +3,12 @@ import { useEffect, useState } from "react";
 
 export type Point = { x: number; y: number; z?: number };
 
-export type Polygon = Point[];
+export type Path = Point[];
+
+export type Polygon = {
+  outer: Path;
+  holes: Path[];
+}
 
 export type Bounds = { minx: number, miny: number, maxx: number, maxy: number };
 
@@ -36,7 +41,7 @@ function Ground({ bounds }: { bounds: Bounds }) {
   return (
     <mesh
       rotation={[-Math.PI / 2, 0, 0]} // rotate plane to lie flat
-      position={[(maxx + minx) / 2, -0.1, (maxy + miny) / 2]}
+      position={[(maxx + minx) / 2, -0.5, (maxy + miny) / 2]} // slightly lower than 0, to prevent depth issues
     >
       <planeGeometry args={[sizex, sizey]} />
       <meshStandardMaterial color="lightgreen" />
@@ -44,18 +49,35 @@ function Ground({ bounds }: { bounds: Bounds }) {
   );
 }
 
+function polygonToShape(polygon: Polygon) {
+  const shape = new three.Shape();
+
+  const outer = polygon.outer;
+  shape.moveTo(outer[0].x, outer[0].y);
+  for (let i = 1; i < outer.length; i++) {
+    shape.lineTo(outer[i].x, outer[i].y);
+  }
+  shape.closePath();
+
+  // add holes
+  polygon.holes.forEach(hole => {
+    const path = new three.Path();
+    path.moveTo(hole[0].x, hole[0].y);
+    for (let i = 1; i < hole.length; i++) {
+      path.lineTo(hole[i].x, hole[i].y);
+    }
+    path.closePath();
+    shape.holes.push(path);
+  });
+
+  return shape;
+}
+
 function RoadPolygons({ polygons }: { polygons: Polygon[] }) {
   return (
     <>
       {polygons.map((poly, idx) => {
-        // create a Shape for the polygon
-        const shape = new three.Shape();
-        poly.forEach((p, i) => {
-          if (i === 0) shape.moveTo(p.x, p.y);
-          else shape.lineTo(p.x, p.y);
-        });
-        shape.closePath();
-
+        const shape = polygonToShape(poly);
         const geometry = new three.ShapeGeometry(shape);
 
         return (
