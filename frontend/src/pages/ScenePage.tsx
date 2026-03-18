aeb088d4f6d540bd5f8526404f75f07a9ca53d2f
Jeroen van Riel <jeroenvanriel@outlook.com>
2026-03-18 09:26:01 +0100

    add sidebar toggle button

--- frontend/src/pages/ScenePage.tsx
+++ frontend/src/pages/ScenePage.tsx
@@ -1,6 +1,7 @@
 import { useParams, Link } from "react-router-dom";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { ArrowLeftIcon } from "@heroicons/react/24/solid";
+import { ChevronLeftIcon, ChevronRightIcon } from "@heroicons/react/24/solid";
 
 import Scene from "../components/Scene";
 import CanvasRecorderPanel from "../gui/CanvasRecorderPanel";
@@ -20,6 +21,7 @@ export default function ScenePage() {
   const { sceneId } = useParams();
   if (!sceneId) return null;
 
+  const [showSidebar, setShowSidebar] = useState(true);
   const load = useReplayController((s) => s.load);
 
   useEffect(() => {
@@ -32,7 +34,9 @@ export default function ScenePage() {
 
   return (
     <div className="flex h-screen w-screen overflow-hidden">
+
       {/* Sidebar */}
+      {showSidebar && (
       <div className="p-5 bg-gray-800 text-white">
         <div className="w-80 flex flex-col space-y-4">
           <Link to="/" className="mb-4 grey-link-button">
@@ -46,11 +50,22 @@ export default function ScenePage() {
           <BufferDebugList />
         </div>
       </div>
+      )}
 
-      {/* Visualization */}
-      <div className="flex-1">
+      {/* Main Area */}
+      <div className="flex-1 relative">
+        {/* Toggle Button */}
+        <button
+          onClick={() => setShowSidebar(!showSidebar)}
+          className="absolute top-4 z-10 text-white bg-gray-800 p-2 rounded-tr-lg rounded-br-lg cursor-pointer"
+        >
+          {showSidebar ? <ChevronLeftIcon className="w-4 h-auto mr-1" /> : <ChevronRightIcon className="w-4 h-auto mr-1" />}
+        </button>
+
+        {/* Visualization */}
         <Scene sceneId={sceneId} />
       </div>
+
     </div>
   )
 }
