df1e1c5040b497cf7894ba359dc0d2938b9dfac0
Jeroen van Riel <jeroenvanriel@outlook.com>
2026-03-21 12:16:21 +0100

    fix camera center after window resize
    Problem was caused by the fact that the parent of the <Canvas> element had some minimum width/height, so that the canvas would stretch outside the actual browser window.

--- frontend/src/pages/ScenePage.tsx
+++ frontend/src/pages/ScenePage.tsx
@@ -53,7 +53,7 @@ export default function ScenePage() {
       )}
 
       {/* Main Area */}
-      <div className="flex-1 relative">
+      <div className="flex-1 relative min-h-0 min-w-0">
         {/* Toggle Button */}
         <button
           onClick={() => setShowSidebar(!showSidebar)}
